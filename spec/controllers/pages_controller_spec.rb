d3653ad5f68cd86404bcb592d3fadbaaf7027d09
Swetha Ganesan <swethaganesan@outlook.com>
2014-05-24 22:12:46 +0800

    everything else

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
     end
-  end
+
+end
 
   describe "GET 'contact'" do
     it "returns http success" do
@@ -15,5 +17,10 @@ describe PagesController do
       response.should be_success
     end
   end
-
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
 end
